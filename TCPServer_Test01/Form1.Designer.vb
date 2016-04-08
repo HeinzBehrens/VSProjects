4153a7f3e10e5954c9a8c26613c275472730c128
HeinzBehrens <HeinzBehrens@web.de>
2016-04-08 12:44:44 +0200

    Anfang mit GitHub

---
+++ TCPServer_Test01/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnStartListener = New System.Windows.Forms.Button()
+        Me.rtbxInfo = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(466, 263)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(81, 23)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnStartListener
+        '
+        Me.btnStartListener.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnStartListener.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStartListener.Location = New System.Drawing.Point(341, 263)
+        Me.btnStartListener.Name = "btnStartListener"
+        Me.btnStartListener.Size = New System.Drawing.Size(119, 23)
+        Me.btnStartListener.TabIndex = 1
+        Me.btnStartListener.Text = "Start Listener"
+        Me.btnStartListener.UseVisualStyleBackColor = True
+        '
+        'rtbxInfo
+        '
+        Me.rtbxInfo.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.rtbxInfo.Location = New System.Drawing.Point(12, 12)
+        Me.rtbxInfo.Name = "rtbxInfo"
+        Me.rtbxInfo.Size = New System.Drawing.Size(535, 245)
+        Me.rtbxInfo.TabIndex = 2
+        Me.rtbxInfo.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(154, 274)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(559, 298)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.rtbxInfo)
+        Me.Controls.Add(Me.btnStartListener)
+        Me.Controls.Add(Me.btnExit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnStartListener As Button
+    Friend WithEvents rtbxInfo As RichTextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ TCPServer_Test01/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Threading
+Imports HBE_API_2013
+
+Public Class Form1
+
+    Private Class test
+
+        Dim _ctrlInfo As clHbeControl
+
+        Public zaehler As Integer = 1
+        Sub testroutine()
+            _ctrlInfo.AppendTextNL(zaehler.ToString("00") & " Dies ist ein Test aus der Testroutine")
+            zaehler += 1
+        End Sub
+
+        Sub New(ctrlinfo As clHbeControl)
+            _ctrlInfo = ctrlinfo
+        End Sub
+
+    End Class
+
+
+
+    Dim infoBox As clHbeControl
+    Public timerFeeder As System.Timers.Timer
+    Private testcl As test
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        infoBox = New clHbeControl(Me.rtbxInfo)
+        infoBox.SetText("Listener Programm gestartet")
+
+        ' nun meine Testklasse einrichten
+        testcl = New test(infoBox)
+
+
+        ' Timer starten mit Testroutine
+
+        ' jetzt sagen wir ihm, was er zu tun hat
+        timerFeeder = New System.Timers.Timer()
+
+        AddHandler timerFeeder.Elapsed, AddressOf testcl.testroutine
+        timerFeeder.SynchronizingObject = Me
+        timerFeeder.Interval = 1 * 1000
+        timerFeeder.AutoReset = True
+        timerFeeder.Start()
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim myThread As New Thread(AddressOf testcl.testroutine)
+
+        myThread.Start()
+
+    End Sub
+End Class
